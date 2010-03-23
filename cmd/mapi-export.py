042a06a7bfba0a96ce78cd5c0719905ff3993059
Avery Pennarun <apenwarr@gmail.com>
2010-03-23 03:07:44 -0400

    Add a mapi-export command.
    It works like crm-export, but exports from a MAPI folder into the gitdb.
    
    Now all we need is a merge and an update and we'll be set!  Probably!

---
+++ cmd/mapi-export.py
@@ -0,0 +1,73 @@
+from lib import options, cmapi, gitdb, entry
+from lib.cmapitags import *
+
+optspec = """
+crampi mapi-export <folder name>
+--
+d,gitdb=   name of gitdb sqlite3 database file
+"""
+
+_mapping = {
+    PR_GIVEN_NAME_W: 'firstname',
+    PR_SURNAME_W: 'lastname',
+    PR_TITLE_W: 'title',
+    PR_DEPARTMENT_NAME_W: 'department',
+    # 0x81ae Email1Address: 'email',
+    # 0x81b3 Email2Address: 'email2',
+    PR_BUSINESS_TELEPHONE_NUMBER_W: 'phone',
+    PR_MOBILE_TELEPHONE_NUMBER_W: 'mobile',
+    PR_BUSINESS_FAX_NUMBER_W: 'fax',
+    PR_BUSINESS_HOME_PAGE_W: 'blog',
+    PR_BIRTHDAY: 'birthdate',
+}
+_admapping = {
+    PR_BUSINESS_ADDRESS_STREET_W: 'street1',
+    PR_BUSINESS_ADDRESS_CITY_W: 'city',
+    PR_BUSINESS_ADDRESS_STATE_OR_PROVINCE_W: 'state',
+    PR_BUSINESS_ADDRESS_POSTAL_CODE_W: 'zip',
+    PR_BUSINESS_ADDRESS_COUNTRY_W: 'country',
+    PR_POSTAL_ADDRESS_W: 'fulladdress',
+}
+
+
+def entries(f):
+    keys = [PR_ENTRYID] + _mapping.keys() + _admapping.keys()
+    for m in f.messages().iter(*keys):
+        d = {}
+        ad = {}
+        for k,kk in _mapping.items():
+            d[kk] = m.get(k)
+        for k,kk in _admapping.items():
+            ad[kk] = m.get(kk)
+        if filter(None, ad.values()):
+            ad['type'] = 'Business'
+            d['addresses'] = [ad]
+        yield entry.Entry(m.get(PR_ENTRYID), None, d)
+
+
+def main(argv):
+    o = options.Options('crampi mapi-export', optspec)
+    (opt, flags, extra) = o.parse(argv[1:])
+
+    if len(extra) != 1:
+       o.fatal('you must provide exactly one folder name')
+    fname = extra[0]
+
+    if not opt.gitdb:
+        opt.gitdb = 'gitdb.sqlite3'
+
+    g = gitdb.GitDb(opt.gitdb)
+    sess = cmapi.Session()
+    f = sess.recursive_find(fname)
+    el = entry.Entries(entries(f))
+    
+    el.uuids_from_commit(g, 'mapi')
+    el.assign_missing_uuids(g)
+    t = el.save_tree(g)
+    print t
+    el2 = entry.load_tree(g, t)
+    t2 = el.save_tree(g)
+    assert(t == t2)
+    print el.save_commit(g, 'mapi')
+    g.flush()
+    
